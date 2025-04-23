0af729a7adca1c5ac3bff307d026df9a55c98d63
Thomas Capelle <tcapelle@pm.me>
2025-04-23 10:52:08 +0000

    update weights

--- axolotl_dev/rewards.py
+++ axolotl_dev/rewards.py
@@ -26,7 +26,7 @@ REWARD_MAGNITUDES = {
     "one_code_blob_not_ok": -0.2,
     "code_runs_fail": -0.25,
     "code_runs_mismatch": 0.2,
-    "code_runs_match": 0.5,
+    "code_runs_match": 1.0,
     "imports_decorator_ok": 0.2,
     "constexpr_ok": 0.2,
     "valid_tl_methods_ok": 0.2,
