12c40e1ebec76062714e7787ea8bf5ccd5bbf592
Thomas Capelle <tcapelle@pm.me>
2025-06-08 21:08:34 +0000

    delete files

--- scripts/push_to_hub.py
+++ scripts/push_to_hub.py
@@ -5,26 +5,37 @@ from huggingface_hub import upload_folder, HfApi
 
 import wandb
 
-api = HfApi(token=os.environ["HF_API_TOKEN"])
 
 @dataclass
 class ScriptArgs:
     artifact: str = "grpo-cuda/axolotl-sft/model-sft-qwen3-14b-v2-instruct:latest"
     model_name: str = "tcapelle/axolotl-sft-qwen3-14b-instruct"
-
+    folder: str = None
 
 args: ScriptArgs = sp.parse(ScriptArgs)
 
-run = wandb.init()
-artifact = run.use_artifact(args.artifact, type="model")
-artifact_dir = artifact.download()
+if args.folder:
+    import torch
+    from transformers import AutoModelForCausalLM, AutoTokenizer
+    model = AutoModelForCausalLM.from_pretrained(args.folder, torch_dtype=torch.bfloat16)
+    tokenizer = AutoTokenizer.from_pretrained(args.folder)
+    model.save_pretrained(args.folder)
+    tokenizer.save_pretrained(args.folder)
+    model.push_to_hub(args.model_name)
+    tokenizer.push_to_hub(args.model_name)
+else:
+    api = HfApi(token=os.environ["HF_API_TOKEN"])
 
-print(f"Downloaded artifact to {artifact_dir}")
-# api.create_repo(args.model_name, repo_type="model")
+    run = wandb.init()
+    artifact = run.use_artifact(args.artifact, type="model")
+    artifact_dir = artifact.download()
 
-upload_folder(
-    repo_id=args.model_name,
-    folder_path=artifact_dir,
-    commit_message="Uploaded from W&B",
-    repo_type="model",
-)
+    print(f"Downloaded artifact to {artifact_dir}")
+    # api.create_repo(args.model_name, repo_type="model")
+
+    upload_folder(
+        repo_id=args.model_name,
+        folder_path=artifact_dir,
+        commit_message="Uploaded from W&B",
+        repo_type="model",
+    )
