c9307c84cf4dada568e876187d805cc91af6654b
Thomas Capelle <tcapelle@pm.me>
2025-06-23 14:42:01 +0000

    update to default env var

--- verifiers_dev/triton_rewards_modular.py
+++ verifiers_dev/triton_rewards_modular.py
@@ -343,7 +343,9 @@ class TritonAPIRubric(Rubric):
         
         return base_reward + performance_reward + memory_reward
 
-def get_triton_env(dataset, triton_server_url: str = RUN_TRITON_ENDPOINT) -> SingleTurnEnv:
+def get_triton_env(dataset, triton_server_url) -> SingleTurnEnv:
+    if triton_server_url is None:
+        triton_server_url = RUN_TRITON_ENDPOINT
     parser = XMLParser(['think', 'triton'], answer_field='triton')
     static_rubric = create_static_rubric(parser)
     api_rubric = TritonAPIRubric(parser, triton_server_url)
